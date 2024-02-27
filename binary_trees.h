5ab0dd92f66a4f18a73314e0c75371c8126fd971
Ghofranahmed1 <gofranahmed40@gmail.com>
2024-02-27 01:10:35 -0800

    task 0

--- binary_trees.h
+++ binary_trees.h
@@ -3,7 +3,7 @@
 
 #include <stdio.h>
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -21,4 +21,6 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
+void binary_tree_print(const binary_tree_t *);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 #endif
