1dcecbb43e4c5d771d036143b6984c74c4422da4
Ghofranahmed1 <gofranahmed40@gmail.com>
2024-02-28 14:30:21 -0800

    tasl 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,9 +1,27 @@
-#include " binary_trees.h"
+#include "binary_trees.h"
+
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: pointer to the root node of the tree to measure the height
+ * Return: height of the tree, or 0 if tree is NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	int left_h, right_h;
+
+	if (tree == NULL)
+		return (0);
+	left_h = binary_tree_height(tree->left);
+	right_h = binary_tree_height(tree->right);
+	return (left_h > right_h ? left_h + 1 : right_h + 1);
+}
 /**
  * binary_tree_balance - func measures the balance factor of tree
  * @tree: tree to be measured
  * Return: the k
  */
+
 int binary_tree_balance(const binary_tree_t *tree)
 {
 	int left_h, right_h;
