693b21800ff6d4f3269ebb3d3bdd5f2172205dc1
Ghofranahmed1 <gofranahmed40@gmail.com>
2024-02-27 22:51:21 -0800

    task 10

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_depth -  func measures the depth of a node in a binary tree
+ * @tree: the binary tree
+ * Return: the deptht or 0 if the tree==null
+ */
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t depth = 0;
+
+	while (tree->parent)
+	{
+		depth++;
+		tree = tree->parent;
+	}
+	return (depth);
+}
